ffb7c3660c6c924c279af29e02a2b4e5b418ab44
Steven Angles <sangles@sonatype.com>
2020-12-19 15:11:18 -0500

    remove errant license headers

--- src/main/java/io/github/stangles/Day2.java
+++ src/main/java/io/github/stangles/Day2.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
- * "Sonatype" is a trademark of Sonatype, Inc.
- */
 package io.github.stangles;
 
 import java.net.URISyntaxException;
